6ef9ac56eef64aa5b27cbd42563dda6aaa4db54d
Victor C. Van Hee <vvanhee@gmail.com>
2026-04-20 19:46:21 -0500

    Initial Commit

---
+++ WCCombine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WCCombine
+//
+//  Created by Victor Van Hee on 4/20/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WCCombine/WCCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  WCCombineApp.swift
+//  WCCombine
+//
+//  Created by Victor Van Hee on 4/20/26.
+//
+
+import SwiftUI
+
+@main
+struct WCCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
